6e1824f5dc298457c8def50429541a11fc2af008
Avinash Tiwari <qwe123kids@gmail.com>
2018-04-01 16:26:49 -0600

    Adding compotent

--- Angular5/1/my-first-app/src/app/app.module.ts
+++ Angular5/1/my-first-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/Forms';
 
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
