9eeb52e7bb6d5bb19d2232be727ded8567e34518
Avinash Tiwari <qwe123kids@gmail.com>
2018-04-28 17:19:49 -0600

    no message

--- Angular5/3/databinding/src/app/app.component.ts
+++ Angular5/3/databinding/src/app/app.component.ts
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{type:'server', name:'Test Server', content:'Just a test'}];
   
- 
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBluePrintAdded(blupePrintdat: {serverName: string, serverContent: string}) {
+  this.serverElements.push({
+      type: 'blueprint',
+      name: blupePrintdat.serverName,
+      content: blupePrintdat.serverContent
+    });
+  }
+
 }
