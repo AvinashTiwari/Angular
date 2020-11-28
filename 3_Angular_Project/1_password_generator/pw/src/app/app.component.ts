69a58a5a83a4de72461e1c89eb378e129eaffe7b
AvinashTiwari85 <qwe123kids@gmail.com>
2020-11-28 15:12:21 -0700

    no message

--- 3_Angular_Project/1_password_generator/pw/src/app/app.component.ts
+++ 3_Angular_Project/1_password_generator/pw/src/app/app.component.ts
@@ -8,14 +8,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   password ='';
   includeLetters = false;
-
+  includeNumbers = false;
+  includeSymbols = false;
   onChangeUseLetters(){
 
     this.includeLetters= !this.includeLetters;
 
   }
+
+  onChangeUseNumbers(){
+    this.includeNumbers= !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+
+    this.includeSymbols= !this.includeSymbols;
+  }
+
   onButtonClick(){
     console.log(this.includeLetters);
+    console.log(this.includeNumbers);
+    console.log(this.includeSymbols);
+    console.log(`About to generate password 
+         Include  Letters :  ${this.includeLetters}
+         Include  Numbers : ${this.includeNumbers}
+         Include  Symbol : ${this.includeSymbols}
+    `);
+    
     this.password = 'My password';
   };
 
