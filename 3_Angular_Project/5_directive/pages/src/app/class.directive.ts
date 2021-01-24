80fca6762c0ed22b07df173ea281eba9b3a74588
AvinashTiwari85 <qwe123kids@gmail.com>
2021-01-24 10:19:58 -0700

    no message

--- 3_Angular_Project/5_directive/pages/src/app/class.directive.ts
+++ 3_Angular_Project/5_directive/pages/src/app/class.directive.ts
@@ -3,13 +3,14 @@ import { Directive , ElementRef, Input} from '@angular/core';
 @Directive({
   selector: '[appClass]'
 })
-export class ClassDirective {
-  @Input() backgroundColor: string; 
+export class ClassDirective { 
 
   constructor(private element: ElementRef) { 
-    setTimeout(() =>{
-     this.element.nativeElement.style.backgroundColor = this.backgroundColor;
-    },50);
+   
+  }
+
+  @Input() set backgroundColor(color: string){
+    this.element.nativeElement.style.backgroundColor = color;
   }
 
 }
