dd1e145cb39e303c7374370cf31000e7c52fd5c1
AvinashTiwari85 <qwe123kids@gmail.com>
2021-02-25 14:27:28 -0700

    no message

--- 3_Angular_Project/8_Lazing_loading_With_Module/Lazing_loading_Module/src/app/shared/divider/divider.component.ts
+++ 3_Angular_Project/8_Lazing_loading_With_Module/Lazing_loading_Module/src/app/shared/divider/divider.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./divider.component.css']
 })
 export class DividerComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
