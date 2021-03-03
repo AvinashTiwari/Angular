efd72bd94ccaef6a0e7111c8bd9c6892c57229ea
AvinashTiwari85 <qwe123kids@gmail.com>
2021-03-03 15:54:50 -0700

    no message

--- 3_Angular_Project/8_Lazing_loading_With_Module/Lazing_loading_Module/src/app/collections/collections-home/collections-home.component.ts
+++ 3_Angular_Project/8_Lazing_loading_With_Module/Lazing_loading_Module/src/app/collections/collections-home/collections-home.component.ts
@@ -7,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
   data = [
-    {name: 'James', age:24 , job:'designer'},
-    {name: 'Jill', age:25, job:'Engineer'},
-    {name: 'Elys', age:26, job: 'Engineer'}];
+    {name: 'James', age:24 , job:'designer', employed: true},
+    {name: 'Jill', age:25, job:'Engineer', employed: false},
+    {name: 'Elys', age:26, job: 'Engineer', employed: true}];
   
-  headers =[{key:'name', label:'Name'},
+  headers =[
+    {key:'employed', label:'Has a Job?'},
+    {key:'name', label:'Name'},
            {key:'age', label:'Age'},
            {key:'job', label:'Job'}];
 
