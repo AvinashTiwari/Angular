c9a31a6c9fd09c3b95a1731a0b1d549c827bb777
AvinashTiwari85 <qwe123kids@gmail.com>
2021-03-14 12:50:51 -0600

    no message

--- 3_Angular_Project/9_Advancement_Compotent_Routing/src/app/app-routing.module.ts
+++ 3_Angular_Project/9_Advancement_Compotent_Routing/src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ const routes: Routes = [
    import('./collections/collections.module').then((m) => m.CollectionsModule)},
   {path: 'elements', loadChildren: ()=>
    import('./elements/elements.module').then((m) => m.ElementsModule)},
+   {path: 'views', loadChildren: ()=>
+   import('./views/views.module').then((m) => m.ViewsModule)},
   {path: '', component: HomeComponent},
   {path:'**', component:NotFoundComponent}
 ];
