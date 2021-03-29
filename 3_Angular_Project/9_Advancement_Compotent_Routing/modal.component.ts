758994ddb9212edc2966ad7e216d21c932646c8f
AvinashTiwari85 <qwe123kids@gmail.com>
2021-03-29 13:46:56 -0600

    no message

--- 3_Angular_Project/9_Advancement_Compotent_Routing/modal.component.ts
+++ 3_Angular_Project/9_Advancement_Compotent_Routing/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,11 +6,9 @@ import { Component, OnInit,ElementRef } from '@angular/core';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
-
   constructor(private el: ElementRef) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
     document.body.appendChild(this.el.nativeElement);
   }
-
 }
