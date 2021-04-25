13a64efe22daa7847947ef4fe3f4dd28fc9d7f0b
AvinashTiwari85 <qwe123kids@gmail.com>
2021-04-25 12:03:31 -0600

    no message

---
+++ 3_Angular_Project/10_ts/index8.ts
@@ -0,0 +1,29 @@
+class Car {
+  public color: string;
+  private year: number;
+
+  constructor(color: string, year: number) {
+    this.color = color;
+    this.year = year;
+  }
+
+  drive() {
+    this.putInGear();
+    this.pressPedal();
+    this.turnWheel();
+  }
+
+  private putInGear() {}
+
+  private pressPedal() {}
+
+  private turnWheel() {}
+}
+
+const myCar = new Car('red', 2000);
+myCar.drive();
+
+myCar.drive();
+myCar.putInGear();
+
+console.log(myCar.color, myCar.year); // 'red', 2000
