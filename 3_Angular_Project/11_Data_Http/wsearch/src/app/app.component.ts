8cbd6448a19aa7d2fea91d30154d7fae51ac1bd2
AvinashTiwari85 <qwe123kids@gmail.com>
2021-05-16 13:56:12 -0600

    no message

--- 3_Angular_Project/11_Data_Http/wsearch/src/app/app.component.ts
+++ 3_Angular_Project/11_Data_Http/wsearch/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-
+import { WikipediaService } from './wikipedia.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private wikipedia: WikipediaService) {}
+
   title = 'wsearch';
   onTerm(term: string) {
-    console.log('I am the app and here is the term', term);
+    const results = this.wikipedia.search(term);
+    console.log(results);
   }
 }
