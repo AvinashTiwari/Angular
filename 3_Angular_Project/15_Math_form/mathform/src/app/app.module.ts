049a03f70d4b4c01061766681ff989bc9ec0f7c6
AvinashTiwari85 <qwe123kids@gmail.com>
2021-08-06 16:43:26 -0600

    no message

--- 3_Angular_Project/15_Math_form/mathform/src/app/app.module.ts
+++ 3_Angular_Project/15_Math_form/mathform/src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EquationComponent } from './equation/equation.component';
+import { AnswerHighlightDirective } from './answer-highlight.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EquationComponent, AnswerHighlightDirective],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
